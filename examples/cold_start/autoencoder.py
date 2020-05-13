fc55d4b348e846a4f7495f35bcc66ba48c270cb2
snubeaver <snubeaver@github.com>
2020-05-13 14:23:19 +0900

    mask

--- examples/cold_start/autoencoder.py
+++ examples/cold_start/autoencoder.py
@@ -29,24 +29,14 @@ class Encoder(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Encoder, self).__init__()
         self.conv1 = GCNConv(in_channels, 2 * out_channels, cached=True)
-        if args.model in ['GAE']:
-            self.conv2 = GCNConv(2 * out_channels, out_channels, cached=True)
-        elif args.model in ['VGAE']:
-            self.conv_mu = GCNConv(2 * out_channels, out_channels, cached=True)
-            self.conv_logvar = GCNConv(
-                2 * out_channels, out_channels, cached=True)
+        self.conv2 = GCNConv(2 * out_channels, out_channels, cached=True)
 
     def forward(self, x, edge_index):
         row, col = x.size()
         mask = torch.zeros(col)
         pdb.set_trace()
-        # x[row-1] = mask
         x = F.relu(self.conv1(x, edge_index))
-        if args.model in ['GAE']:
-            return self.conv2(x, edge_index)
-        elif args.model in ['VGAE']:
-            return self.conv_mu(x, edge_index), self.conv_logvar(x, edge_index)
-
+        return self.conv2(x, edge_index)
 
 channels = 16
 dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
